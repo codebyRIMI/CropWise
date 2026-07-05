491d43bcd379758533fbd639098a4135f708c998
Aritra Manna <aritramanna924@gmail.com>
2026-07-05 08:53:05 +0530

    changes urls

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -14,9 +14,6 @@ urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-<<<<<<< HEAD
-]
-=======
     path('forgot-password', ForgotPasswordView.as_view(), name='forgot-password'),
     path('reset-password', ResetPasswordView.as_view(), name='reset-password'),
     path('register-request', RegisterRequestView.as_view()),
@@ -26,4 +23,3 @@ urlpatterns = [
         name="verify-email"
     )
 ]
->>>>>>> 4056922a66f416de61d9290e027a64967c5d184d
