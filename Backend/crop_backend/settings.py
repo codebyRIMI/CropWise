ffcae33007f1f30c60c54b6ca9061396b189df99
codebyRIMI <rimihalder1603@gmail.com>
2026-07-14 21:34:56 +0530

    errorrrrr

--- Backend/crop_backend/settings.py
+++ Backend/crop_backend/settings.py
@@ -49,9 +49,9 @@ FRONTEND_URL = os.getenv("FRONTEND_URL")
 INSTALLED_APPS = [
     "daphne",
     'channels',
-
-    # "cloudinary_storage",
     "cloudinary",
+    "cloudinary_storage",
+    
     "analytics",
 
     'django.contrib.admin',
@@ -236,20 +236,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 
-# STORAGES = {
-#     "default": {
-#         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-#     },
-#     "staticfiles": {
-#         # "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"
-#         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-#     },
-# }
-
 
 STORAGES = {
     "default": {
-        "BACKEND": "cloudinary.storage.MediaCloudinaryStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
